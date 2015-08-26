5865008c3b3bbea08a9b198d1e2227789dcf50eb
Robert Tayco <rob@roberttayco.com>
2015-08-25 18:31:08 -0700

    chat rooms work

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,27 @@
 blocTalk = angular.module('BlocTalk', ['ui.router', 'firebase']);
 
-var ref = new Firebase('https://bloctalk.firebaseio.com/');
-
 blocTalk.config(['$stateProvider', '$locationProvider', function($stateProvider, $locationProvider) {
    $locationProvider.html5Mode({
       enabled: true
    });
+
+   // Home view
+   $stateProvider.state('home', {
+      url: '/',
+      controller: 'HomeController',
+      templateUrl: '/templates/home.html'
+   });
 }]);
 
-// blocTalk.controller('MainController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-//    
-// }]);
+blocTalk.factory('Room', ['$firebaseArray', function($firebaseArray) {
+   var $firebaseRef = new Firebase('https://bloctalk.firebaseio.com/');
+   var rooms = $firebaseArray($firebaseRef.child('rooms'));
+
+   return {
+      all: rooms
+   };
+}]);
+
+blocTalk.controller('HomeController', ['$scope', 'Room', function($scope, Room) {
+
+}]);
