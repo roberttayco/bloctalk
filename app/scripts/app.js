e812bab385cd02128171340c1104c99481a148fe
Robert Tayco <rob@roberttayco.com>
2015-08-25 18:53:42 -0700

    syncing up room-factory branch

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,8 +6,25 @@ blocTalk.config(['$stateProvider', '$locationProvider', function($stateProvider,
    $locationProvider.html5Mode({
       enabled: true
    });
+   $stateProvider.state('main', {
+      url: '/',
+      controller: 'Main.controller',
+      templateUrl: '/templates/home.html'
+   });
 }]);
 
 // blocTalk.controller('MainController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-//    
+//
 // }]);
+
+blocTalk.factory('Room', ['$firebaseArray', function($firebaseArray) {
+   var rooms = $firebaseArray(ref);
+
+   return {
+      all: rooms
+   };
+}]);
+
+blocTalk.controller('Main.controller', ['$scope', 'Room', function($scope, Room) {
+   $scope.rooms = Room.all;
+}]);
