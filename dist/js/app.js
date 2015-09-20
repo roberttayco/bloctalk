b752b139450f2917e12ec7bf442c216772450754
Robert Tayco <rob@roberttayco.com>
2015-09-20 14:24:30 -0700

    fixed modal to add rooms

--- dist/js/app.js
+++ dist/js/app.js
@@ -13,8 +13,8 @@ blocTalk.config(['$stateProvider', '$locationProvider', function($stateProvider,
 }]);
 
 blocTalk.factory('Room', ['$firebaseArray', function($firebaseArray) {
-   var fbRef = new Firebase('https://bloctalk.firebaseio.com/');
-   var rooms = $firebaseArray(fbRef);
+   var ref   = new Firebase('https://bloctalk.firebaseio.com/');
+   var rooms = $firebaseArray(ref);
    return {
       all: rooms
    };
@@ -22,6 +22,24 @@ blocTalk.factory('Room', ['$firebaseArray', function($firebaseArray) {
 
 blocTalk.controller('Main.controller', ['$scope', 'Room', '$modal', function($scope, Room, $modal) {
    $scope.rooms = Room.all;
+   $scope.currentRoom = null;
+
+   $scope.changeRoom = function (room) {
+      $scope.currentRoom = room;
+   };
+
+   $scope.addMessage = function () {
+      var text = $scope.newMessageText;
+
+      var message = {
+
+      };
+
+      $scope.currentRoom.messages.push(message);
+      $scope.rooms.$save($scope.currentRoom);
+   };
+
+   $scope.animationsEnabled = true;
 
    $scope.open = function() {
       var modalInstance = $modal.open({
@@ -33,5 +51,16 @@ blocTalk.controller('Main.controller', ['$scope', 'Room', '$modal', function($sc
 }]);
 
 blocTalk.controller('AddRoom.controller', ['$scope', '$modalInstance', 'Room', function($scope, $modalInstance, Room) {
+   $scope.addNewRoom = function() {
 
+      var room = {
+         name: $scope.newRoomName
+      };
+
+      Room.all.$add(room).then(function(ref) {
+         room.$id = ref.key();
+      });
+
+      $modalInstance.close();
+   };
 }]);
